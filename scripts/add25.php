4b0d53eb38cfd61a7bccc0faf124f11f7260debd
wswapped <wswapped@gmail.com>
2018-06-14 15:38:05 +0200

    sa

--- scripts/add25.php
+++ scripts/add25.php
@@ -1,16 +1,16 @@
 <?php
 include "../db.php";
+die("Take care");
+// $query = $db->query("SELECT * FROM users");
+// while ($data = $query->fetch_assoc()) {
+// 	$phone = $data['phone'];
+// 	if(strlen($phone) == 10){
+// 		//updating
+// 		$nphone = '25'.$phone;
 
-$query = $db->query("SELECT * FROM users");
-while ($data = $query->fetch_assoc()) {
-	$phone = $data['phone'];
-	if(strlen($phone) == 10){
-		//updating
-		$nphone = '25'.$phone;
-
-		//updating
-		$sql = "UPDATE users SET phone = '$nphone' WHERE id = ".$data['id'];
-		echo "$sql<br />";
-		$db->query($sql);
-	}
-}
+// 		//updating
+// 		$sql = "UPDATE users SET phone = '$nphone' WHERE id = ".$data['id'];
+// 		echo "$sql<br />";
+// 		$db->query($sql);
+// 	}
+// }
