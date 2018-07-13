77c7907e2baf79ba0de8e4e1f2d3f846a21d5805
wswapped <wswapped@gmail.com>
2018-07-13 20:29:06 +0200

    Agasimba

--- churches/api/index.php
+++ churches/api/index.php
@@ -293,6 +293,9 @@
 			$message = $request['message']??"";
 			$token = $request['token']??"";
 
+			$phone 	= preg_replace( '/[^0-9]/', '', $phone );
+			$phone 	= substr($phone, -12);
+
 			if(strlen($phone) ==  10){
 				$phone = '25'.$phone;
 			}
@@ -300,7 +303,8 @@
 			if($token == 'ireebe'){
 				if($phone && $message){
 					//Sending the message
-					$url = "http://rslr.connectbind.com:8080/bulksms/bulksms?username=infk-kiza&password=lab250&type=0&dlr=1&destination=$phone&source=Ireebe&message=$message";
+					$url = "http://rslr.connectbind.com:8080/bulksms/bulksms?username=infk-kiza&password=lab250&type=0&dlr=1&destination=$phone&source=ireebe&message=$message";
+					// echo "$url";
 					// Get cURL resource
 					$curl = curl_init();
 					// Set some options - we are passing in a useragent too here
