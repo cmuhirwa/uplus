2277e27615dc1e2b8d40c0ff55bbaf09cc7358e6
Clement <muhirwaclement@gmail.com>
2018-08-12 15:25:24 +0200

    Update invest.php

--- api/invest.php
+++ api/invest.php
@@ -252,9 +252,7 @@
 			//make the user like
 			$investDb->query("INSERT INTO feed_likes(feedCode, userCode) VALUES(\"$feedId\", \"$userId\")");
 			echo json_encode("Done");
-		}
-		
-		
+		}	
 	}
 
 	function listCommentsFeed()
@@ -273,9 +271,7 @@
 			echo json_encode($comments);			
 		}else{
 			echo json_encode("Fail");
-		}
-
-		
+		}	
 	}
 
 	function commentFeed()
@@ -443,6 +439,9 @@
 			        }
 	        	}
 	        }
+
+	        //Notify Users
+	        notification($post_content);
 
             $response = 'Done';
         }else{
@@ -1286,4 +1285,53 @@
 		echo json_encode($data);		
 	}
 // END INVESTMENT
+
+// START NOTIFICATION
+	function notification($message)
+	{
+		require('db.php');
+
+		// CLEAN MESSAGE TO TITLE
+		$message = $message = substr($message, -20);
+
+		function Send_notifications ($tokens, $message)
+		{
+			$url = 'https://fcm.googleapis.com/fcm/send';
+			$fields = array(
+				 'registration_ids' => $tokens,
+				 'data' => $message
+				);
+			$headers = array(
+				'Authorization:key = AAAAO68is70:APA91bENIQEx0MyqjxnDzuVpNIZZQDP_H9EpkkP5U594EUefXUxKLUxxuwWXJalyfwNpKyiv3QtV3wKqFDJYzXpc7PlpMiiTkZ2m-dvOj_Ora5GrBVwJH7_liT7psj-KUEl9_s53NFbe',
+				'Content-Type: application/json'
+			);
+		   $ch = curl_init();
+	       curl_setopt($ch, CURLOPT_URL, $url);
+	       curl_setopt($ch, CURLOPT_POST, true);
+	       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	       curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);  
+	       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	       curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+	       $result = curl_exec($ch);           
+	       if ($result === FALSE) {
+	           die('Curl failed: ' . curl_error($ch));
+	       }
+	       curl_close($ch);
+	       return $result;
+		}	
+		
+		$sql = $db->query("SELECT investToken FROM users");
+		$tokens = array();
+		if(mysqli_num_rows($sql) > 0 ){
+			while ($row = mysqli_fetch_assoc($sql)) {
+				$tokens[] = $row["token"];
+			}
+		}
+		mysqli_close($db);
+		$message = array("message" => $message);
+		$message_status = Send_notifications($tokens, $message);
+		header('Content-Type: application/json');
+	}
+// END NOTIFICATION
 ?>
