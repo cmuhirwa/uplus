3823193f2933d2223daddf180ef87b588e3a9586
Clement <muhirwaclement@gmail.com>
2018-08-12 15:43:31 +0200

    Update invest.php

--- api/invest.php
+++ api/invest.php
@@ -441,7 +441,7 @@
 	        }
 
 	        //Notify Users
-	     //   notification($post_content);
+	        notification('TestMessage');
 
             $response = 'Done';
         }else{
@@ -1287,52 +1287,52 @@
 // END INVESTMENT
 
 // START NOTIFICATION
-	function notification($messageing)
+function notification($premessage)
+{
+	require('db.php');
+
+	// CLEAN MESSAGE TO TITLE
+	$premessage = substr($premessage, -30);
+
+	function Send_notifications ($tokens, $message)
 	{
-		require('db.php');
-
-		// CLEAN MESSAGE TO TITLE
-		$messageing = $messageing = substr($messageing, -20);
-
-		function Send_notifications ($tokens, $message)
-		{
-			$url = 'https://fcm.googleapis.com/fcm/send';
-			$fields = array(
-				 'registration_ids' => $tokens,
-				 'data' => $message
-				);
-			$headers = array(
-				'Authorization:key = AAAAO68is70:APA91bENIQEx0MyqjxnDzuVpNIZZQDP_H9EpkkP5U594EUefXUxKLUxxuwWXJalyfwNpKyiv3QtV3wKqFDJYzXpc7PlpMiiTkZ2m-dvOj_Ora5GrBVwJH7_liT7psj-KUEl9_s53NFbe',
-				'Content-Type: application/json'
+		$url = 'https://fcm.googleapis.com/fcm/send';
+		$fields = array(
+			 'registration_ids' => $tokens,
+			 'data' => $message
 			);
-		   $ch = curl_init();
-	       curl_setopt($ch, CURLOPT_URL, $url);
-	       curl_setopt($ch, CURLOPT_POST, true);
-	       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	       curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);  
-	       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	       curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-	       $result = curl_exec($ch);           
-	       if ($result === FALSE) {
-	           die('Curl failed: ' . curl_error($ch));
-	       }
-	       curl_close($ch);
-	       return $result;
-		}	
-		
-		$sql = $db->query("SELECT investToken FROM users WHERE investToken IS NOT NULL");
-		$tokens = array();
-		if(mysqli_num_rows($sql) > 0 ){
-			while ($row = mysqli_fetch_assoc($sql)) {
-				$tokens[] = $row["token"];
-			}
+		$headers = array(
+			'Authorization:key = AAAAO68is70:APA91bENIQEx0MyqjxnDzuVpNIZZQDP_H9EpkkP5U594EUefXUxKLUxxuwWXJalyfwNpKyiv3QtV3wKqFDJYzXpc7PlpMiiTkZ2m-dvOj_Ora5GrBVwJH7_liT7psj-KUEl9_s53NFbe',
+			'Content-Type: application/json'
+		);
+	   $ch = curl_init();
+       curl_setopt($ch, CURLOPT_URL, $url);
+       curl_setopt($ch, CURLOPT_POST, true);
+       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);  
+       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+       curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+       $result = curl_exec($ch);           
+       if ($result === FALSE) {
+           die('Curl failed: ' . curl_error($ch));
+       }
+       curl_close($ch);
+       return $result;
+	}	
+	
+	$sql = $db->query("SELECT investToken FROM users");
+	$tokens = array();
+	if(mysqli_num_rows($sql) > 0 ){
+		while ($row = mysqli_fetch_assoc($sql)) {
+			$tokens[] = $row["investToken"];
 		}
-		mysqli_close($db);
-		$message = array("message" => $messageing);
-		$message_status = Send_notifications($tokens, $message);
-		header('Content-Type: application/json');
-		echo $message_status;
 	}
+	mysqli_close($db);
+	$message = array("message" => $premessage);
+	$message_status = Send_notifications($tokens, $message);
+	header('Content-Type: application/json');
+	echo $message_status;
+}
 // END NOTIFICATION
 ?>
