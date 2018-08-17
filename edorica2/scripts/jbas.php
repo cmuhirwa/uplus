69e7e8e9b8d2ec2c84812a1578c454251c9ece92
wswapped <wswapped@gmail.com>
2018-08-17 09:40:16 +0200

    lk

--- edorica2/scripts/jbas.php
+++
@@ -1,2 +0,0 @@
-"code"=>"Code", "name"=>'Name', "division"=>'Division', 'gender'=>'Gender', 'school'=>"School", 
-"code"=>"Code", "name"=>'Name', "division"=>'Division', 'gender'=>'Gender', 'school'=>"School", 
