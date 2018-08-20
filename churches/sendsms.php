abee20f703d249bb0b0d0aa7c382a0af0f74e2e0
wswapped <wswapped@gmail.com>
2018-08-20 13:14:19 +0200

    ok

--- churches/sendsms.php
+++ churches/sendsms.php
@@ -1,17 +1,18 @@
 <?php
     include_once '../db.php';
-    die("Hello guys, take care");
+    // die("Hello guys, take care");
     $users = $db->query("SELECT * FROM sms.inyatsi");
     set_time_limit(0);
 
     while ($data = $users->fetch_assoc()) {
         $phone = "25".$data['phone'];
-        $message = "Mutoni Doreen anejejwe no kubatumira mu nama ya nyuma itegura ubukwe bwe iraba uyu munsi taliki ya 09/08/2018 kuri hilltop hotel saa 5:00PM, 0783669599.";
+        $message = "Umuryango wa Muhozi Richard na Mutoni Doreen tubikuye ku mutima turabashimira kutuba hafi mu bukwe bwacu n'urukundo mwatugararije nukuri byaratunejeje kubana namwe. Tubifurije umugisha uva ku  Mana.";
         
         if(sendsms($phone, $message)){
             echo "$phone<br />";
         };
     }
+
     function sendsms($phone, $message){
         // $url = "http://rslr.connectbind.com:8080/bulksms/bulksms?username=infk-kiza&password=lab250&type=0&dlr=1&destination=$phone&source=MUTONI%20D&message=".rawurlencode($message);
         $url = "http://162.243.131.210/playsms/index.php?app=ws&u=ireebe&h=bee712b8c43c079fc7fe757b4309a2d7&op=pv&to=$phone&msg=".rawurlencode($message);
