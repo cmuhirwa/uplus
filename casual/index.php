94839ef32013b03d47d96897c0b3241c4681e28c
wswapped <wswapped@gmail.com>
2018-08-20 13:53:26 +0200

    tt

--- casual/index.php
+++ casual/index.php
@@ -1,3 +1,11 @@
 <?php
- header("location: public/index.php"); 
+
+$conn = mysqli_connect("localhost","clement","clement123","epiz_22468831_bazimya");
+if(isset($_GET["hh"])){
+      $data = $_GET["hh"];
+      $sql = "INSERT INTO `epiz_22468831_bazimya`.`datas` (`dat_id`, `dat_`) VALUES (NULL, '$data')";
+      $res = mysqli_query($conn,$sql);
+} else {
+        echo " not set";
+         }
 ?>
